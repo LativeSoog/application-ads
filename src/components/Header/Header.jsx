292555c0ca2ef64adcc1952606368005597002da
Vitaliy <konfet88@gmail.com>
2023-11-16 02:29:27 +0600

    Added the add ad modal window

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { Authorization } from '../Authorization/Authorization'
 import * as S from './style'
+import { AddAdvert } from '../ModalsAdvert/AddAdvert'
 
 export const Header = () => {
   const [openAuthForm, setOpenAuthForm] = useState(false)
+  const [openAddAdvert, setOpenAddAdvert] = useState(false)
   const [isLogin, setIsLogin] = useState(true)
 
   const handleAuthMode = () => {
@@ -11,23 +13,28 @@ export const Header = () => {
     document.body.style.overflow = 'hidden'
   }
 
+  const handleAddAdvert = () => {
+    setOpenAddAdvert(true)
+    document.body.style.overflow = 'hidden'
+  }
+
   const closeWindow = () => {
     setOpenAuthForm(false)
+    setOpenAddAdvert(false)
     document.body.style.overflow = 'unset'
   }
 
   return (
     <>
       {openAuthForm ? <Authorization closeWindow={closeWindow} /> : ''}
+      {openAddAdvert ? <AddAdvert closeWindow={closeWindow} /> : ''}
       <S.Header>
         <S.HeaderNav>
           {isLogin ? (
             <>
-              <S.HeaderButtonLink to="/profile">
-                <S.HeaderButtonMain $width="232px">
-                  Разместить объявление
-                </S.HeaderButtonMain>
-              </S.HeaderButtonLink>
+              <S.HeaderButtonMain $width="232px" onClick={handleAddAdvert}>
+                Разместить объявление
+              </S.HeaderButtonMain>
               <S.HeaderButtonLink to="/profile">
                 <S.HeaderButtonMain $width="173px" $marginLeft="10px">
                   Личный кабинет
