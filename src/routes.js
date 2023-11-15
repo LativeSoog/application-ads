7eee919d6c77052b0a6379243f77027504a90d08
Vitaliy <konfet88@gmail.com>
2023-11-16 03:12:18 +0600

    Added layout of my ad page

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { PageLayout } from './components/PageLayout/PageLayout'
 import { ProfilePage } from './page/Profile/ProfilePage'
 import { AdvertPage } from './page/Advert/AdvertPage'
 import { ProfileSellerPage } from './page/Profile-Seller/ProfileSellerPage'
+import { MyAdvertPage } from './page/My-Advert/MyAdvertPage'
 
 export const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ export const AppRoutes = () => {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile-seller" element={<ProfileSellerPage />} />
         <Route path="/advert" element={<AdvertPage />} />
+        <Route path="/my-advert" element={<MyAdvertPage />} />
       </Route>
     </Routes>
   )
