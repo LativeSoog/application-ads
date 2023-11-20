5d8ee61141061abc2a1909d812e7e08c29cbb6a9
Vitaliy <konfet88@gmail.com>
2023-11-21 03:10:40 +0600

    Implemented the display of information about the seller received from the backend

--- src/page/Profile-Seller/ProfileSellerPage.jsx
+++ src/page/Profile-Seller/ProfileSellerPage.jsx
@@ -1,9 +1,28 @@
 import { useParams } from 'react-router-dom'
 import { CardItem } from '../../components/CardItem/CardItem'
 import * as S from './style'
+import { useGetAllAdvertsQuery } from '../../services/advert'
+import { useEffect, useState } from 'react'
+import { formatDate } from '../../helper'
+import { ButtonPhone } from '../../components/ButtonPhoneAdvert/ButtonPhone'
 
 export const ProfileSellerPage = () => {
+  const host = 'http://127.0.0.1:8090/'
   const params = useParams()
+  const [profileSeller, setProfileSeller] = useState(false)
+
+  const { data: usersAdvertData } = useGetAllAdvertsQuery()
+
+  useEffect(() => {
+    if (usersAdvertData) {
+      const findUser = usersAdvertData.find(
+        (userAdvert) => String(userAdvert.user.id) === params.id,
+      )
+      setProfileSeller(findUser.user)
+    }
+  })
+
+  console.log(profileSeller)
 
   return (
     <S.MainContainer>
@@ -27,24 +46,25 @@ export const ProfileSellerPage = () => {
               <S.ProfileSellerLeft>
                 <S.ProfileSellerImgBlock>
                   <S.ProfileLink>
-                    <S.ProfileSellerImgBlockImage src="" />
+                    <S.ProfileSellerImgBlockImage
+                      src={
+                        profileSeller.avatar ? host + profileSeller.avatar : ''
+                      }
+                    />
                   </S.ProfileLink>
                 </S.ProfileSellerImgBlock>
               </S.ProfileSellerLeft>
 
               <S.ProfileSellerRight>
-                <S.ProfileSellerTitle>Кирилл Матвеев</S.ProfileSellerTitle>
-                <S.ProfileSellerCity>Астана</S.ProfileSellerCity>
+                <S.ProfileSellerTitle>
+                  {profileSeller.name}
+                </S.ProfileSellerTitle>
+                <S.ProfileSellerCity>{profileSeller.city}</S.ProfileSellerCity>
                 <S.ProfileSellerInfo>
-                  Продает товары с августа 2021
+                  Продает товары с {formatDate(profileSeller.sells_from)}
                 </S.ProfileSellerInfo>
 
-                <S.ProfileSellerBtn>
-                  Показать&nbsp;телефон
-                  <S.ProfileSellerBtnSpan>
-                    8&nbsp;905&nbsp;ХХХ&nbsp;ХХ&nbsp;ХХ
-                  </S.ProfileSellerBtnSpan>
-                </S.ProfileSellerBtn>
+                <ButtonPhone userPhoneNumber={profileSeller.phone} />
               </S.ProfileSellerRight>
             </S.ProfileSeller>
           </S.ProfileContent>
