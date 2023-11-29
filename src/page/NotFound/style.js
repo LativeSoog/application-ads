28cb520b4df6669aa407b9467e693b0379de6ac8
Vitaliy <konfet88@gmail.com>
2023-11-29 06:08:25 +0600

    Added page 404

---
+++ src/page/NotFound/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  max-width: 1178px;
+  margin: 0 auto;
+  padding: 0px 5px;
+`
+
+export const MainBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const MainImage = styled.img`
+  width: 300px;
+  height: 300px;
+`
+
+export const MainTitle = styled.h3`
+  font-size: 32px;
+  line-height: 46px;
+  font-weight: 500;
+  color: #000;
+  margin-top: 32px;
+  padding: 0 5px;
+`
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ import { ProfilePage } from './page/Profile/ProfilePage'
 import { AdvertPage } from './page/Advert/AdvertPage'
 import { ProfileSellerPage } from './page/Profile-Seller/ProfileSellerPage'
 import { MyAdvertPage } from './page/My-Advert/MyAdvertPage'
+import { NotFound } from './page/NotFound/NotFound'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<PageLayout />}>
+        <Route path="*" element={<NotFound />} />
         <Route index element={<MainPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile-seller/:id" element={<ProfileSellerPage />} />
